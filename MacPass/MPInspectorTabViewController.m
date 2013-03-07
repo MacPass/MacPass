93e61210f4dc279f1c1681b7155795fc8cc00e6a
michael starke <michael.starke@hicknhack-software.com>
2013-03-07 03:49:43 +0100

    Sketched new Inspector view

--- MacPass/MPInspectorTabViewController.m
+++ MacPass/MPInspectorTabViewController.m
@@ -12,6 +12,9 @@
 
 @property (assign) IBOutlet NSImageView *itemImageView;
 @property (assign) IBOutlet NSTextField *itemNameTextfield;
+@property (assign) IBOutlet NSTabView *tabView;
+@property (assign) IBOutlet NSSegmentedControl *tabControl;
+@property (assign) NSUInteger selectedIndex;
 
 @end
 
@@ -24,9 +27,15 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-
+      // init
     }
     return self;
+}
+
+- (void)didLoadView {
+  //[self.tabView bind:NSSelectedIndexBinding toObject:self.tabControl withKeyPath:@"selectedIndex" options:nil];
+  [self.tabControl bind:NSSelectedIndexBinding toObject:self withKeyPath:NSSelectedIndexBinding options:nil];
+  [self.tabView bind:NSSelectedIndexBinding toObject:self withKeyPath:NSSelectedIndexBinding options:nil];
 }
 
 @end
