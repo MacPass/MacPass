3278c42e9c78e4c838b7d3caaa4e214fb0a968b7
michael starke <michael.starke@hicknhack-software.com>
2013-03-11 00:50:04 +0100

    Introduced Custom Image View for editing-popup

---
+++ MacPass/MPIconSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  MPIconSelectViewController.h
+//  MacPass
+//
+//  Created by Michael Starke on 10.03.13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "MPViewController.h"
+
+@interface MPIconSelectViewController : MPViewController
+
+@end
