a1633ad6ba0efcd31cc2b42ea8edce186492065b
michael starke <michael.starke@hicknhack-software.com>
2013-06-20 20:55:47 +0200

    Updated HNHUi

---
+++ MacPass/MPFirstResonderNotification.h
@@ -0,0 +1,13 @@
+//
+//  MPFirstResonderNotification.h
+//  MacPass
+//
+//  Created by Michael Starke on 20.06.13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MPFirstResonderNotification <NSObject>
+
+@end
