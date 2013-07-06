005bab738024cbc365a0edd0e8e29a41a5c47f9f
michael starke <michael.starke@hicknhack-software.com>
2013-07-06 04:16:56 +0200

    Updated HNHUi

---
+++ MacPass/MPPasswordStringFormatter.h
@@ -0,0 +1,13 @@
+//
+//  MPPasswordStringFormatter.h
+//  MacPass
+//
+//  Created by Michael Starke on 06.07.13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPPasswordStringFormatter : NSFormatter
+
+@end
---
+++ MacPass/MPPasswordStringFormatter.m
@@ -0,0 +1,13 @@
+//
+//  MPPasswordStringFormatter.m
+//  MacPass
+//
+//  Created by Michael Starke on 06.07.13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "MPPasswordStringFormatter.h"
+
+@implementation MPPasswordStringFormatter
+
+@end
