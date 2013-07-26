592069781168ee28bd9be0b79113e9413570acf1
michael starke <michael.starke@hicknhack-software.com>
2013-07-27 00:18:58 +0200

    Removed obsolte Files Added ToolbarButton to be used as context-menu

---
+++ MacPass/MPContextToolbarButton.h
@@ -0,0 +1,14 @@
+//
+//  MPSegmentedToolbarButton.h
+//  MacPass
+//
+//  Created by Michael Starke on 26.07.13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MPContextToolbarButton : NSSegmentedControl
+
+- (void)setImage:(NSImage *)image;
+@end
---
+++ MacPass/MPContextToolbarButton.m
@@ -0,0 +1,46 @@
+//
+//  MPSegmentedToolbarButton.m
+//  MacPass
+//
+//  Created by Michael Starke on 26.07.13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "MPContextToolbarButton.h"
+
+@implementation MPContextToolbarButton
+
+- (id)initWithFrame:(NSRect)frame {
+  self = [super initWithFrame:frame];
+  if (self) {
+    [self setFocusRingType:NSFocusRingTypeNone];
+    [self setSegmentCount:1];
+    //[[self cell] setWidth:15 forSegment:1];
+    [[self cell] setTrackingMode:NSSegmentSwitchTrackingMomentary];
+    [self setSegmentStyle:NSSegmentStyleTexturedSquare];
+  }
+  
+  return self;
+}
+/*
+ Block the segment setter to prevent accidential settings
+ */
+- (void)setImage:(NSImage *)image forSegment:(NSInteger)segment {
+  if(segment == 0) {
+    [super setImage:image forSegment:segment];
+  }
+}
+
+- (void)setSegmentCount:(NSInteger)count {
+  if(count == 1) {
+    [super setSegmentCount:count];
+  }
+}
+
+- (void)setImage:(NSImage *)image {
+  [self setImage:image forSegment:0];
+}
+
+
+
+@end
