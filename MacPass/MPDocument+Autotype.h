2c8dba441a3e4fabfc15b767f14552de76da2d52
michael starke <michael.starke@hicknhack-software.com>
2013-11-04 00:24:21 +0100

    Updated KeePassKit (updated documentation)

---
+++ MacPass/MPDocument+Autotype.h
@@ -0,0 +1,13 @@
+//
+//  MPDocument+Autotype.h
+//  MacPass
+//
+//  Created by Michael Starke on 01/11/13.
+//  Copyright (c) 2013 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "MPDocument.h"
+
+@interface MPDocument (Autotype)
+
+@end
