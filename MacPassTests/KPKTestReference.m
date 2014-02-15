edd2d89d3073afef05a568f33c569ea7987bda59
michael starke <michael.starke@hicknhack-software.com>
2014-02-15 18:35:13 +0100

    Placeholder substation reworked

---
+++ MacPassTests/KPKTestReference.m
@@ -0,0 +1,34 @@
+//
+//  KPKTestReference.m
+//  MacPass
+//
+//  Created by Michael Starke on 15.02.14.
+//  Copyright (c) 2014 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KPKTestReference : XCTestCase
+
+@end
+
+@implementation KPKTestReference
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
