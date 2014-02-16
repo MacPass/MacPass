530bb7ce30c915eb2a290e0d9b040d6b294a134b
michael starke <michael.starke@hicknhack-software.com>
2014-02-17 00:09:25 +0100

    Extended NSUUID extension Reference Retrieval now can be used with "undashed" ID strings Added NSUUID+KeePassKit Test

--- MacPassTests/KPKTestReference.m
+++ MacPassTests/KPKTestReference.m
@@ -7,6 +7,10 @@
 //
 
 #import <XCTest/XCTest.h>
+
+#import "KPKTree.h"
+#import "KPKGroup.h"
+#import "KPKEntry.h"
 #import "NSString+Commands.h"
 
 @interface KPKTestReference : XCTestCase
@@ -16,8 +20,40 @@
 @implementation KPKTestReference
 
 - (void)testCorrectReference {
-  NSString *reference = @"This is some nice stuff {REF:T@U:blubber} and another Reference {REF:U@I:2687345AASTA}";
-  [reference resolveReferencesWithTree:nil];
+  KPKTree *tree = [[KPKTree alloc] init];
+  KPKGroup *group = [[KPKGroup alloc] init];
+  KPKEntry *entry1 = [[KPKEntry alloc] init];
+  KPKEntry *entry2 = [[KPKEntry alloc] init];
+  entry1.title = @"-Entry1Title-";
+  NSString *title2 = [[NSString alloc] initWithFormat:@"Nothing{REF:T@I:%@}Changed", entry1.uuid.UUIDString];
+  entry2.title = title2;
+  entry2.url = @"-Entry2URL-";
+  
+  [group addEntry:entry1];
+  [group addEntry:entry2];
+  tree.root = group;
+  
+  NSString *result = [entry2.title resolveReferencesWithTree:tree];
+  XCTAssertTrue([result isEqualToString:@"Nothing-Entry1Title-Changed"], @"Replaced Strings should match");
+}
+
+- (void)testRecursiveReference{
+  KPKTree *tree = [[KPKTree alloc] init];
+  KPKGroup *group = [[KPKGroup alloc] init];
+  KPKEntry *entry1 = [[KPKEntry alloc] init];
+  KPKEntry *entry2 = [[KPKEntry alloc] init];
+  NSString *title1 = [[NSString alloc] initWithFormat:@"Title1{REF:A@I:%@}", entry2.uuid.UUIDString];
+  entry1.title = title1; // References URL of entry 2
+  NSString *title2 = [[NSString alloc] initWithFormat:@"Nothing{REF:T@I:%@}Changed", entry1.uuid.UUIDString];
+  entry2.title = title2; // Refernces Title of entry 1
+  entry2.url = @"-Entry2URL-";
+  
+  [group addEntry:entry1];
+  [group addEntry:entry2];
+  tree.root = group;
+  
+  NSString *result = [entry2.title resolveReferencesWithTree:tree];
+  XCTAssertTrue([result isEqualToString:@"NothingTitle1-Entry2URL-Changed"], @"Replaced Strings should match");
 }
 
 - (void)testWrongRefernceFormat {
