5f84dfe10038c6a5e7e2f3d1236690658483d04c
michael starke <michael.starke@hicknhack-software.com>
2014-09-01 19:03:37 +0200

    Updated to current KeePassKit

---
+++ MacPass/MPTreeDelegate.h
@@ -0,0 +1,13 @@
+//
+//  MPTreeDelegate.h
+//  MacPass
+//
+//  Created by Michael Starke on 01/09/14.
+//  Copyright (c) 2014 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPTreeDelegate : NSObject
+
+@end
---
+++ MacPass/MPTreeDelegate.m
@@ -0,0 +1,13 @@
+//
+//  MPTreeDelegate.m
+//  MacPass
+//
+//  Created by Michael Starke on 01/09/14.
+//  Copyright (c) 2014 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "MPTreeDelegate.h"
+
+@implementation MPTreeDelegate
+
+@end
