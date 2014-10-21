a8a4ee4d53fddfeb1b82feefe172463aaf0dff10
michael starke <michael.starke@hicknhack-software.com>
2014-10-21 16:25:52 +0200

    Updated submodulse to current stat

---
+++ MacPass/MPTargetItemResolving.h
@@ -0,0 +1,13 @@
+//
+//  MPTargetItemResolving.h
+//  MacPass
+//
+//  Created by Michael Starke on 21/10/14.
+//  Copyright (c) 2014 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MPTargetItemResolving <NSObject>
+
+@end
