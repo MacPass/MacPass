04b2d89fb6a0838788cf22db70800837524da367
michael starke <michael.starke@hicknhack-software.com>
2015-11-09 13:54:10 +0100

    Added test databases (copied from KeePassKit)
    Signed-off-by: michael starke <michael.starke@hicknhack-software.com>

--- MacPassTests/MPDatabaseLoading.m
+++ MacPassTests/MPDatabaseLoading.m
@@ -19,7 +19,7 @@
 
 
 - (void)testLoadVersion1 {
-  NSBundle *myBundle = [NSBundle bundleForClass:[KPKTree class]];
+  NSBundle *myBundle = [NSBundle bundleForClass:[self class]];
   NSURL *url = [myBundle URLForResource:@"Test_Password_1234" withExtension:@"kdb"];
   NSError *error = nil;
   MPDocument *document = [[MPDocument alloc] initWithContentsOfURL:url ofType:@"kdb" error:&error];
@@ -33,7 +33,7 @@
 }
 
 - (void)testVersion1WrongPassword {
-  NSBundle *myBundle = [NSBundle bundleForClass:[KPKTree class]];
+  NSBundle *myBundle = [NSBundle bundleForClass:[self class]];
   NSURL *url = [myBundle URLForResource:@"Test_Password_1234" withExtension:@"kdb"];
   NSError *error = nil;
   MPDocument *document = [[MPDocument alloc] initWithContentsOfURL:url ofType:@"kdb" error:&error];
@@ -45,7 +45,7 @@
 }
 
 - (void)testLoadDatabaseVerions2 {
-  NSBundle *myBundle = [NSBundle bundleForClass:[KPKTree class]];
+  NSBundle *myBundle = [NSBundle bundleForClass:[self class]];
   NSURL *url = [myBundle URLForResource:@"Test_Password_1234" withExtension:@"kdbx"];
   NSError *error = nil;
   MPDocument *document = [[MPDocument alloc] initWithContentsOfURL:url ofType:@"kdbx" error:&error];
