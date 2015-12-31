06d24f4870de099a4993bb6a151530b9a9350042
michael starke <michael.starke@hicknhack-software.com>
2015-12-31 10:39:04 +0100

    Adding Autotype delay test case

---
+++ MacPassTests/MPTestAutotypeDelay.m
@@ -0,0 +1,39 @@
+//
+//  MPTestAutotypeDelay.m
+//  MacPass
+//
+//  Created by Michael Starke on 28/12/15.
+//  Copyright © 2015 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MPTestAutotypeDelay : XCTestCase
+
+@end
+
+@implementation MPTestAutotypeDelay
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
