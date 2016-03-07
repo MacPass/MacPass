d408c925bfeddf25dc5649d09469e56e5d6f4a0d
michael starke <michael.starke@hicknhack-software.com>
2016-03-07 19:55:35 +0100

    Stubbed entry proxy to enable better model editing behaviour

---
+++ MacPass/MPEntryProxy.m
@@ -0,0 +1,36 @@
+//
+//  MPEntryProxy.m
+//  MacPass
+//
+//  Created by Michael Starke on 07/03/16.
+//  Copyright © 2016 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "MPEntryProxy.h"
+#import <KeePassKit/KeePassKit.h>
+
+@interface MPEntryProxy ()
+
+@property (strong) KPKEntry *entry;
+
+@end
+
+@implementation MPEntryProxy
+
+- (instancetype)initWithEntry:(KPKEntry *)entry {
+  if(!entry) {
+    @throw [NSException exceptionWithName:NSInvalidArgumentException reason:nil userInfo:nil];
+  }
+  _entry = entry;
+  return self;
+}
+
+- (void)forwardInvocation:(NSInvocation *)invocation {
+  [invocation invokeWithTarget:self.entry];
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)sel {
+  return [self.entry methodSignatureForSelector:sel];
+}
+
+@end
