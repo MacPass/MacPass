1c39f1c3fed0cc22577c949df7aff632f46958d1
michael starke <michael.starke@hicknhack-software.com>
2016-09-01 19:52:34 +0200

    using newer KeePassKit with correct undo/redo support expiration

---
+++ MacPass/MPAutotypeBuilderViewController.m
@@ -0,0 +1,22 @@
+//
+//  MPAutotypeBuilderViewController.m
+//  MacPass
+//
+//  Created by Michael Starke on 01/09/16.
+//  Copyright © 2016 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "MPAutotypeBuilderViewController.h"
+
+@interface MPAutotypeBuilderViewController ()
+
+@end
+
+@implementation MPAutotypeBuilderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
