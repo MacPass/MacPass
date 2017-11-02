2a1e88a97a324099b920b07b9df90905f92cfb3f
michael starke <michael.starke@hicknhack-software.com>
2017-11-02 17:57:01 +0100

    fixed menu item duplication for NSSecureTextFields

--- MacPass/MPEntryInspectorViewController.m
+++ MacPass/MPEntryInspectorViewController.m
@@ -516,7 +516,11 @@ typedef NS_ENUM(NSUInteger, MPEntryTab) {
 #pragma mark -
 #pragma mark HNHUITextFieldDelegate
 - (NSMenu *)textField:(NSTextField *)textField textView:(NSTextView *)view menu:(NSMenu *)menu {
-  
+  for(NSMenuItem *item in [menu itemArray]) {
+    if(item.action == @selector(showReferenceBuilder:)) {
+      return menu; // item is already contained!
+    }
+  }
   NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"SHOW_REFERENCE_BUILDER", @"Menu item to show the reference builder in a text view's context menu")action:@selector(showReferenceBuilder:) keyEquivalent:@""];
   item.representedObject = textField;
   [menu addItem:item];
