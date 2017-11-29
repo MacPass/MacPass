5db3950424f19520d0f1b13f7d7647dab01adf80
Michael Starke <michael.starke@hicknhack-software.com>
2017-11-29 14:37:13 +0100

    fixed issue with wrong clear timeout when pasting was done inside the clear timeout

---
+++ MacPass/MPPluginConstants.h
@@ -0,0 +1,13 @@
+//
+//  MPPluginConstants.h
+//  MacPass
+//
+//  Created by Michael Starke on 29.11.17.
+//  Copyright © 2017 HicknHack Software GmbH. All rights reserved.
+//
+
+#ifndef MPPluginConstants_h
+#define MPPluginConstants_h
+
+
+#endif /* MPPluginConstants_h */
---
+++ MacPass/MPPluginConstants.m
@@ -0,0 +1,9 @@
+//
+//  MPPluginConstants.m
+//  MacPass
+//
+//  Created by Michael Starke on 29.11.17.
+//  Copyright © 2017 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
