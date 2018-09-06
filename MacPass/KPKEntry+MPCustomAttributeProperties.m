3d89bc75454bac766c99c3a21fdd62a68ca97728
Michael Starke <michael.starke@hicknhack-software.com>
2018-09-06 09:10:56 +0200

    Added stub for custom attribute dynamic lookup

--- MacPass/KPKEntry+MPCustomAttributeProperties.m
+++ MacPass/KPKEntry+MPCustomAttributeProperties.m
@@ -8,6 +8,29 @@
 
 #import "KPKEntry+MPCustomAttributeProperties.h"
 
+NSString *const MPCustomAttributePropertyPrefix = @"valueForCustomAttribute";
+
 @implementation KPKEntry (MPCustomAttributeProperties)
+
+/*- (void)forwardInvocation:(NSInvocation *)anInvocation {
+  NSString *selector = NSStringFromSelector(anInvocation.selector);
+  if([selector hasPrefix:MPCustomAttributePropertyPrefix]) {
+    NSString *key = [selector substringFromIndex:MPCustomAttributePropertyPrefix.length];
+    KPKAttribute *attribute = [self attributeWithKey:key];
+    if(attribute) {
+      anInvocation.selector = @selector(value);
+      [anInvocation invokeWithTarget:attribute];
+    }
+    else {
+      anInvocation.selector = @selector(_unkownCustomAttributeValue);
+      [anInvocation invokeWithTarget:self];
+    }
+  }
+  else [super forwardInvocation:anInvocation];
+}*/
+
+- (NSString *)_unkownCustomAttributeValue {
+  return @"";
+}
 
 @end
