c0c053d4222b4664f0dec64f7862c9d7d6193135
Michael Starke <michael.starke@hicknhack-software.com>
2019-08-27 14:54:00 +0200

    Retain custom file extension if present and do not change back to .kdbx (or .kdb)

--- MacPass/MPDocument.m
+++ MacPass/MPDocument.m
@@ -173,6 +173,18 @@ NSString *const MPDocumentGroupKey                            = @"MPDocumentGrou
   return NO;
 }
 
+- (NSString *)fileNameExtensionForType:(NSString *)typeName saveOperation:(NSSaveOperationType)saveOperation {
+  NSString *proposedExtension = [super fileNameExtensionForType:typeName saveOperation:saveOperation];
+  if(!self.fileURL) {
+    return proposedExtension;
+  }
+  NSString *actualExtension = self.fileURL.pathExtension;
+  if(![actualExtension isEqualToString:proposedExtension]) {
+    NSLog(@"Extension missmatch.");
+  }
+  return actualExtension;
+}
+
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError * _Nullable __autoreleasing *)outError {
   if(self.encrypted) {
     NSLog(@"%@ should not be called on locked databases!", NSStringFromSelector(_cmd));
