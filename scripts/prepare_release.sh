940988794255e5ecba09a796f7a70bcc31c96af2
Michael Starke <michael.starke@hicknhack-software.com>
2020-01-28 09:41:07 +0100

    Introduced direct signing of all parts since --deep did not sign Autoupdate and fileop in Sparkle.framework

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -56,13 +56,35 @@ DERIVED_DATA_FOLDER="${TMPDIR}"
 BUILD_FOLDER="${DERIVED_DATA_FOLDER}"/Build/Products/Release
 APP_BUNDLE=MacPass.app
 APP_BUNDLE_ZIP="${APP_BUNDLE}".zip
+APP_FRAMEWORK_PATH="${APP_BUNDLE}"/Contents/Frameworks
+SPARKLE_FRAMEWORK="${APP_FRAMEWORK_PATH}"/Sparkle.framework
+SPARKLE_AUTOUPDATE_BUNDLE="${SPARKLE_FRAMEWORK}"/Resources/Autoupdate.app
+SPARKLE_FILEOP="${SPARKLE_AUTOUPDATE_BUNDLE}"/Contents/MacOS/fileop
+TRANSFORMERKIT_FRAMEWORK="${APP_FRAMEWORK_PATH}"/TransformerKit.framework
+KEEPASSKIT_FRAMEWORK="${APP_FRAMEWORK_PATH}"/KeePassKit.framework
+KISSXML_FRAMEWORK="${KEEPASSKIT_FRAMEWORK}"/Versions/Current/Frameworks/KissXML.framework
+HNHUI_FRAMEWORK="${APP_FRAMEWORK_PATH}"/HNHUi.framework
 cd ..
 echo "Building..."
 xcodebuild build -configuration Release -project MacPass.xcodeproj -scheme MacPass CODE_SIGNING_REQUIRED=NO -derivedDataPath "${DERIVED_DATA_FOLDER}"
 cd "${BUILD_FOLDER}"
 echo ""
-echo "Signing..."
-codesign --sign "${IDENTITY}" --options runtime --deep --force --entitlements "${ENTITLEMENTS}" "${APP_BUNDLE}"
+echo "Signing Sparkle - fileop..."
+codesign --sign "${IDENTITY}" --options runtime --force --entitlements "${ENTITLEMENTS}" "${SPARKLE_FILEOP}"
+echo "Signing Sparkle - Autoupdate..."
+codesign --sign "${IDENTITY}" --options runtime --force --entitlements "${ENTITLEMENTS}" "${SPARKLE_AUTOUPDATE_BUNDLE}"
+echo "Signing Sparkle..."
+codesign --sign "${IDENTITY}" --options runtime --force --entitlements "${ENTITLEMENTS}" "${SPARKLE_FRAMEWORK}"
+echo "Signing TransformerKit..."
+codesign --sign "${IDENTITY}" --options runtime --force --entitlements "${ENTITLEMENTS}" "${TRANSFORMERKIT_FRAMEWORK}"
+echo "Signing HNHUi..."
+codesign --sign "${IDENTITY}" --options runtime --force --entitlements "${ENTITLEMENTS}" "${HNHUI_FRAMEWORK}"
+echo "Signing KeePassKit - KissXML..."
+codesign --sign "${IDENTITY}" --options runtime --force --entitlements "${ENTITLEMENTS}" "${KISSXML_FRAMEWORK}"
+echo "Signing KeePassKit..."
+codesign --sign "${IDENTITY}" --options runtime --force --entitlements "${ENTITLEMENTS}" "${KEEPASSKIT_FRAMEWORK}"
+echo "Signing MacPass..."
+codesign --sign "${IDENTITY}" --options runtime --force --entitlements "${ENTITLEMENTS}" "${APP_BUNDLE}"
 echo ""
 echo "Archiving..."
 ditto -c -k --keepParent "${APP_BUNDLE}" "${APP_BUNDLE_ZIP}"
