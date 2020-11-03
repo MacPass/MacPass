8e3bca062bbcb30c8bc00b6f8c529ddd9135b8c8
Michael Starke <michael.starke@hicknhack-software.com>
2020-11-03 17:08:09 +0100

    Fixed regression in Preferences preventing willShowTab and didShowTab from being called.

--- MacPass/MPTabViewController.h
+++ MacPass/MPTabViewController.h
@@ -12,6 +12,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface MPTabViewController : NSTabViewController
 
+@property (nonatomic, copy, nullable) void (^willSelectTabHandler)(NSTabViewItem *item);
+@property (nonatomic, copy, nullable) void (^didSelectTabHandler)(NSTabViewItem *item);
+
 @end
 
 NS_ASSUME_NONNULL_END
