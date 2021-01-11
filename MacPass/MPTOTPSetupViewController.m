58399d66be35aedc475a948db6665c66a69a4f82
Michael Starke <michael.starke@hicknhack-software.com>
2021-01-11 15:36:43 +0100

    WIP on setup view update

--- MacPass/MPTOTPSetupViewController.m
+++ MacPass/MPTOTPSetupViewController.m
@@ -18,6 +18,7 @@
 @property (strong) IBOutlet NSPopUpButton *digitCountPopUpButton;
 @property (strong) IBOutlet NSImageView *qrCodeImageView;
 @property (strong) IBOutlet NSGridView *gridView;
+@property (strong) IBOutlet NSPopUpButton *typePopUpButton;
 
 @property (strong) KPKTimeOTPGenerator *generator;
 
@@ -32,13 +33,83 @@ typedef NS_ENUM(NSUInteger, MPOTPUpdateSource) {
   MPOTPUpdateSourceEntry
 };
 
+typedef NS_ENUM(NSUInteger, MPOTPType) {
+  MPOTPTypeRFC,
+  MPOTPTypeSteam,
+  MPOTPTypeCustom
+};
+
 @implementation MPTOTPSetupViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   NSAssert([self.representedObject isKindOfClass:KPKEntry.class], @"represented object needs to be a KPKEntry");
+
+  /* algorithm */
+  NSMenuItem *sha1Item = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"HASH_SHA1", "SHA 1 hash algoritm menu item") action:NULL keyEquivalent:@""];
+  sha1Item.tag = KPKOTPHashAlgorithmSha1;
+  NSMenuItem *sha256Item = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"HASH_SHA256", "SHA 256 hash algoritm menu item") action:NULL keyEquivalent:@""];
+  sha256Item.tag = KPKOTPHashAlgorithmSha256;
+  NSMenuItem *sha512Item = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"HASH_SHA512", "SHA 512 hash algoritm menu item") action:NULL keyEquivalent:@""];
+  sha512Item.tag = KPKOTPHashAlgorithmSha512;
+  
+  NSAssert(self.algorithmPopUpButton.menu.numberOfItems == 0, @"Hash algorithm menu needs to be empty");
+  [self.algorithmPopUpButton.menu addItem:sha1Item];
+  [self.algorithmPopUpButton.menu addItem:sha256Item];
+  [self.algorithmPopUpButton.menu addItem:sha512Item];
+  
+  /* digits */
+  NSAssert(self.digitCountPopUpButton.menu.numberOfItems == 0, @"Digit menu needs to be empty");
+  for(NSUInteger digit = 6; digit <= 8; digit++) {
+    NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:[NSString stringWithFormat:@"%ld", digit] action:NULL keyEquivalent:@""];
+    item.tag = digit;
+    [self.digitCountPopUpButton.menu addItem:item];
+  }
+
+  NSAssert(self.typePopUpButton.menu.numberOfItems == 0, @"Type menu needs to be empty!");
+  NSMenuItem *rfcItem = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"OTP_RFC", @"OTP type RFC ") action:NULL keyEquivalent:@""];
+  rfcItem.tag = MPOTPTypeRFC;
+  NSMenuItem *steamItem = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"OTP_STEAM", @"OTP type Steam ") action:NULL keyEquivalent:@""];
+  steamItem.tag = MPOTPTypeSteam;
+  NSMenuItem *customItem = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"OTP_CUSTOM", @"OTP custom type ") action:NULL keyEquivalent:@""];
+  customItem.tag = MPOTPTypeCustom;
+  
+  [self.typePopUpButton.menu addItem:rfcItem];
+  [self.typePopUpButton.menu addItem:steamItem];
+  [self.typePopUpButton.menu addItem:customItem];
+  
+  [self _updateView:MPOTPUpdateSourceEntry];
+}
+
+- (IBAction)toggleDisclosure:(id)sender {
+  for(NSInteger row = 1; row < self.gridView.numberOfRows; row++) {
+    NSGridRow *gridRow = [self.gridView rowAtIndex:row];
+    gridRow.hidden = !gridRow.hidden;
+  }
+}
+
+- (IBAction)parseQRCode:(id)sender {
+  if(sender != self.qrCodeImageView) {
+    return; // wrong sender
+  }
+  [self _updateView:MPOTPUpdateSourceQRImage];
+}
+
+- (void)_updateView:(MPOTPUpdateSource)source {
   self.generator = [[KPKTimeOTPGenerator alloc] initWithEntry:((KPKEntry *)self.representedObject)];
   
+  if(source == MPOTPUpdateSourceQRImage) {
+    NSString *qrCodeString = self.qrCodeImageView.image.QRCodeString;
+    NSURL *otpURL = [NSURL URLWithString:qrCodeString];
+    if(!otpURL.isTimeOTPURL) {
+      return; // no valid URL
+    }
+    self.urlTextField.stringValue = otpURL.absoluteString;
+  }
+
+  
+  /* FIXME: update correct values based on changes */
+
   /* URL and QR code */
   KPKEntry *entry = self.representedObject;
   NSString *url = [entry attributeWithKey:kKPKAttributeKeyOTPOAuthURL].value;
@@ -56,53 +127,13 @@ typedef NS_ENUM(NSUInteger, MPOTPUpdateSource) {
   /* secret */
   NSString *secret = [self.generator.key base32EncodedString];
   self.secretTextField.stringValue = secret ? secret : @"";
-  
-  /* algorithm */
-  NSMenuItem *sha1Item = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"HASH_SHA1", "SHA 1 hash algoritm menu item") action:NULL keyEquivalent:@""];
-  sha1Item.tag = KPKOTPHashAlgorithmSha1;
-  NSMenuItem *sha256Item = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"HASH_SHA256", "SHA 256 hash algoritm menu item") action:NULL keyEquivalent:@""];
-  sha256Item.tag = KPKOTPHashAlgorithmSha256;
-  NSMenuItem *sha512Item = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"HASH_SHA512", "SHA 512 hash algoritm menu item") action:NULL keyEquivalent:@""];
-  sha512Item.tag = KPKOTPHashAlgorithmSha512;
-  
-  [self.algorithmPopUpButton.menu removeAllItems];
-  [self.algorithmPopUpButton.menu addItem:sha1Item];
-  [self.algorithmPopUpButton.menu addItem:sha256Item];
-  [self.algorithmPopUpButton.menu addItem:sha512Item];
-  
+
   [self.algorithmPopUpButton selectItemWithTag:self.generator.hashAlgorithm];
   
-  /* digits */
-  [self.digitCountPopUpButton.menu removeAllItems];
-  for(NSUInteger digit = 6; digit <= 8; digit++) {
-    NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:[NSString stringWithFormat:@"%ld", digit] action:NULL keyEquivalent:@""];
-    item.tag = digit;
-    [self.digitCountPopUpButton.menu addItem:item];
-  }
   [self.digitCountPopUpButton selectItemWithTag:self.generator.numberOfDigits];
-}
 
-- (IBAction)toggleDisclosure:(id)sender {
-  for(NSInteger row = 1; row < self.gridView.numberOfRows; row++) {
-    NSGridRow *gridRow = [self.gridView rowAtIndex:row];
-    gridRow.hidden = !gridRow.hidden;
-  }
-}
-
-- (IBAction)parseQRCode:(id)sender {
-  if(sender != self.qrCodeImageView) {
-    return; // wrong sender
-  }
-  NSString *qrCodeString = self.qrCodeImageView.image.QRCodeString;
-  NSURL *otpURL = [NSURL URLWithString:qrCodeString];
-  if(!otpURL.isTimeOTPURL) {
-    return; // no valid URL
-  }
-  self.urlTextField.stringValue = otpURL.absoluteString;
-}
-
-- (void)_updateView:(MPOTPUpdateSource)source {
-  // TODO: implement update
+  
+  
 }
 
 @end
