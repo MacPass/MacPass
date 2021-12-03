89268ef88ec5f369a8712113eb9cc6e1294eff75
Michael Starke <michael.starke@hicknhack-software.com>
2021-12-03 20:43:21 +0100

    Made scrollview in groups inspector tab actually work

--- MacPass/MPGroupInspectorViewController.m
+++ MacPass/MPGroupInspectorViewController.m
@@ -62,17 +62,17 @@
   scrollView.showBottomShadow = NO;
   scrollView.hasVerticalRuler = YES;
   scrollView.drawsBackground = NO;
-  scrollView.translatesAutoresizingMaskIntoConstraints = NO;
+  /*scrollView.translatesAutoresizingMaskIntoConstraints = NO;
   NSView *clipView = scrollView.contentView;
   
-  scrollView.documentView = self.contentView;
+  //scrollView.documentView = self.contentView;
   
   NSDictionary *views = NSDictionaryOfVariableBindings(_contentView);
   [clipView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_contentView]|"
                                                                    options:0
                                                                    metrics:nil
                                                                      views:views]];
-  [self.view layoutSubtreeIfNeeded];
+  [self.view layoutSubtreeIfNeeded];*/
   
   NSMenu *autotypeMenu = self.autotypePopupButton.menu;
   NSMenuItem *inheritAutotype = [[NSMenuItem alloc] initWithTitle:NSLocalizedString(@"AUTOTYPE_INHERIT", "Inherit autotype settings menu item") action:NULL keyEquivalent:@""];
