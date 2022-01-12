12a22a369b7d226a9eb65407b0f1ffda1db560c2
George Snow <gsnowiii@gmail.com>
2022-01-11 23:28:06 -0500

    adding funtionality for toggling dock and menubar icons via gui - icons are bound to settings yet

---
+++ MacPass/MPDisplayOptions.h
@@ -0,0 +1,17 @@
+//
+//  MPDisplayOptions.h
+//  MacPass
+//
+//  Created by George Snow on 1/11/22.
+//  Copyright © 2022 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MPDisplayOptions.h"
+
+
+@interface MPDisplayOption : NSObject;
+
+
+
+@end
